c356e90569d897c81992ff343666c02ad3c3b0d0
jessicaborsetto <bjessi2002@gmail.com>
2024-03-06 16:12:03 +0100

    clone

---
+++ hashnode/src/components/atoms/NavBtn.jsx
@@ -0,0 +1,15 @@
+// import { Button } from "@material-tailwind/react";
+ 
+function NavBtn() {
+  return (
+    <>
+      <button className="rounded-full">Filled</button>
+     
+      <button variant="outlined" className="rounded-full">
+        Outlined
+      </button>
+    </>
+  );
+}
+
+export default NavBtn
---
+++ hashnode/src/components/atoms/NavDrop.jsx
@@ -0,0 +1,88 @@
+
+
+
+
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavDrop() {
+    return (
+        <Navbar variant="dark" bg="trasparent" className='p-0'>
+
+
+            <Navbar.Toggle aria-controls="navbar-light-example" />
+            <Navbar.Collapse id="navbar-light-example">
+
+                <NavDropdown
+                    id="nav-dropdown-light-example"
+                    title="More "
+                    menuVariant="light"
+                >
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm '>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-compass" viewBox="0 0 16 16" className='menuIcon'>
+                            <path d="M8 16.016a7.5 7.5 0 0 0 1.962-14.74A1 1 0 0 0 9 0H7a1 1 0 0 0-.962 1.276A7.5 7.5 0 0 0 8 16.016m6.5-7.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0" />
+                            <path d="m6.94 7.44 4.95-2.83-2.83 4.95-4.949 2.83 2.828-4.95z" />
+                        </svg> <p className='dropColor'>Explore</p></NavDropdown.Item>
+
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='menuIcon'>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 6.75 22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3-4.5 16.5" />
+                        </svg>
+                        <p className='dropColor'>Hackathons</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-diagram-2" viewBox="0 0 16 16" className='menuIcon'>
+                            <path fill-rule="evenodd" d="M6 3.5A1.5 1.5 0 0 1 7.5 2h1A1.5 1.5 0 0 1 10 3.5v1A1.5 1.5 0 0 1 8.5 6v1H11a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-1 0V8h-5v.5a.5.5 0 0 1-1 0v-1A.5.5 0 0 1 5 7h2.5V6A1.5 1.5 0 0 1 6 4.5zM8.5 5a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5zM3 11.5A1.5 1.5 0 0 1 4.5 10h1A1.5 1.5 0 0 1 7 11.5v1A1.5 1.5 0 0 1 5.5 14h-1A1.5 1.5 0 0 1 3 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5zm4.5.5a1.5 1.5 0 0 1 1.5-1.5h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1A1.5 1.5 0 0 1 9 12.5zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5z" />
+                        </svg>
+                        <p className='dropColor'>Changelogs</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-broadcast-pin" viewBox="0 0 16 16" className='menuIcon'>
+                            <path d="M3.05 3.05a7 7 0 0 0 0 9.9.5.5 0 0 1-.707.707 8 8 0 0 1 0-11.314.5.5 0 0 1 .707.707m2.122 2.122a4 4 0 0 0 0 5.656.5.5 0 1 1-.708.708 5 5 0 0 1 0-7.072.5.5 0 0 1 .708.708m5.656-.708a.5.5 0 0 1 .708 0 5 5 0 0 1 0 7.072.5.5 0 1 1-.708-.708 4 4 0 0 0 0-5.656.5.5 0 0 1 0-.708m2.122-2.12a.5.5 0 0 1 .707 0 8 8 0 0 1 0 11.313.5.5 0 0 1-.707-.707 7 7 0 0 0 0-9.9.5.5 0 0 1 0-.707zM6 8a2 2 0 1 1 2.5 1.937V15.5a.5.5 0 0 1-1 0V9.937A2 2 0 0 1 6 8" />
+                        </svg>
+                        <p className='dropColor'>The Commit Podcast</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Divider />
+
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16" className='menuIcon'>
+                            <path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
+                            <path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
+                        </svg>
+                        <p className='dropColor'>Download apps</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item href="#action/3.1" className='flex gap-3 py-2 px-4 text-sm'>
+                        <svg viewBox="0 0 337 337" fill="none" className='menuIcon'>
+                            <rect x="113" y="113" width="111" height="111" rx="55.5" fill="white" />
+                            <path fill-rule="evenodd" clip-rule="evenodd" d="M23.155 112.598c-30.873 30.874-30.873 80.93 0 111.804l89.443 89.443c30.874 30.873 80.93 30.873 111.804 0l89.443-89.443c30.873-30.874 30.873-80.93 0-111.804l-89.443-89.443c-30.874-30.873-80.93-30.873-111.804 0l-89.443 89.443zm184.476 95.033c21.612-21.611 21.612-56.651 0-78.262-21.611-21.612-56.651-21.612-78.262 0-21.612 21.611-21.612 56.651 0 78.262 21.611 21.612 56.651 21.612 78.262 0z" fill="#4C5A6D" />
+                        </svg>
+                        <p className='dropColor'>Logos & media</p>
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Divider />
+
+                   
+                        <NavDropdown.Item  className='flex gap-3 py-2 px-4 text-sm'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='menuIcon' >
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M16.712 4.33a9.027 9.027 0 0 1 1.652 1.306c.51.51.944 1.064 1.306 1.652M16.712 4.33l-3.448 4.138m3.448-4.138a9.014 9.014 0 0 0-9.424 0M19.67 7.288l-4.138 3.448m4.138-3.448a9.014 9.014 0 0 1 0 9.424m-4.138-5.976a3.736 3.736 0 0 0-.88-1.388 3.737 3.737 0 0 0-1.388-.88m2.268 2.268a3.765 3.765 0 0 1 0 2.528m-2.268-4.796a3.765 3.765 0 0 0-2.528 0m4.796 4.796c-.181.506-.475.982-.88 1.388a3.736 3.736 0 0 1-1.388.88m2.268-2.268 4.138 3.448m0 0a9.027 9.027 0 0 1-1.306 1.652c-.51.51-1.064.944-1.652 1.306m0 0-3.448-4.138m3.448 4.138a9.014 9.014 0 0 1-9.424 0m5.976-4.138a3.765 3.765 0 0 1-2.528 0m0 0a3.736 3.736 0 0 1-1.388-.88 3.737 3.737 0 0 1-.88-1.388m2.268 2.268L7.288 19.67m0 0a9.024 9.024 0 0 1-1.652-1.306 9.027 9.027 0 0 1-1.306-1.652m0 0 4.138-3.448M4.33 16.712a9.014 9.014 0 0 1 0-9.424m4.138 5.976a3.765 3.765 0 0 1 0-2.528m0 0c.181-.506.475-.982.88-1.388a3.736 3.736 0 0 1 1.388-.88m-2.268 2.268L4.33 7.288m6.406 1.18L7.288 4.33m0 0a9.024 9.024 0 0 0-1.652 1.306A9.025 9.025 0 0 0 4.33 7.288" />
+                            </svg>
+                        
+                                <p className='dropColor'>Support</p>
+                       
+                        </NavDropdown.Item>
+                
+
+
+                </NavDropdown>
+
+            </Navbar.Collapse>
+
+        </Navbar>
+    );
+}
+
+export default NavDrop;
