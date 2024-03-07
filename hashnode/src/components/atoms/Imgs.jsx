f42adda0b99386b234b6dfe27261eee2680f7105
jessicaborsetto <bjessi2002@gmail.com>
2024-03-07 17:43:42 +0100

    clone

---
+++ hashnode/src/components/atoms/Imgs.jsx
@@ -0,0 +1,36 @@
+import first from "C:/Users/Jessica/Desktop/hashnodeClone/hashnode/src/assets/first.avif";
+import second from "C:/Users/Jessica/Desktop/hashnodeClone/hashnode/src/assets/second.avif";
+import third from "C:/Users/Jessica/Desktop/hashnodeClone/hashnode/src/assets/third.png";
+import fourth from "C:/Users/Jessica/Desktop/hashnodeClone/hashnode/src/assets/fourth.avif";
+
+
+function Imgs() {
+    return (
+       
+            <div className="container mx-auto ">
+                <div >
+                    <div className="img">
+                        <img src={third} alt="" className="draw" />
+                    </div>
+                    <div className="img">
+                        <img src={second} alt="" className="center" />
+                    </div>
+                </div>
+
+
+                <div className="flex justify-between lati">
+                    <div className="sx">
+                        <img src={first} alt="" />
+                    </div>
+                    <div className="dx">
+                        <img src={fourth} alt="" />
+                    </div>
+                </div>
+
+
+            </div>
+       
+    )
+}
+
+export default Imgs
--- hashnode/src/pages/Home.jsx
+++ hashnode/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import Header from "../components/Header"
+import Imgs from "../components/atoms/Imgs"
 
 function Home(){
     return(
+        <>
         <Header></Header>
+
+        <Imgs></Imgs>
+    
+        </>
     )
 }
 export default Home
