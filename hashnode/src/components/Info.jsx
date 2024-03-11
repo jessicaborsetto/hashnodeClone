bd99d06b973e78508bc4ca411101f9451787cdd1
jessicaborsetto <bjessi2002@gmail.com>
2024-03-11 16:17:58 +0100

    clone

---
+++ hashnode/src/components/Info.jsx
@@ -0,0 +1,76 @@
+// import first from "C:/Users/Jessica/Desktop/hashnodeClone/hashnode/src/assets/first.avif";
+
+function Info() {
+    return (
+        <>
+            <h2 className="container mx-auto my-40 text-5xl font-semibold text-slate-900 px-64"><span className="text-blue-500">Blogging platform </span>optimized for software developers and technical writers.</h2>
+            <div className="container mx-auto  px-64 grid gap-10">
+                <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7">
+                    <div className="grid grid-cols-2 place-content-center">
+                        <div className="text-left p-10 ">
+                            <h3 className="text-xl font-bold text-slate-800 ">Super-fast and SEO-optimized blogs built for developers and tech enthusiasts.</h3>
+                            <p className="text-md font-md text-slate-500 ">Hashnode blogs are fast, up-to-date with SEO techniques, and score above 90 on all Lighthouse parameters. They're simple, elegant, and user-friendly.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7">
+                    <div className="grid grid-cols-2 place-content-center">
+                        <div className="text-left p-10 ">
+                            <h3 className="text-xl font-bold text-slate-800 ">Own your traffic. Blog for free on a custom domain!</h3>
+                            <p className="text-md font-md text-slate-500 ">Don't blog on rented URLs. Map your custom domain to your Hashnode blog for free and own your traffic. True content ownership.</p>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="grid grid-cols-2 gap-10">
+
+                    <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7">
+                        <div className="grid grid-cols-2 place-content-center">
+                            <div className="text-left p-10 ">
+                                <h3 className="text-xl font-bold text-slate-800 ">Super-fast and SEO-optimized blogs built for developers and tech enthusiasts.</h3>
+                                <p className="text-md font-md text-slate-500 ">Hashnode blogs are fast, up-to-date with SEO techniques, and score above 90 on all Lighthouse parameters. They're simple, elegant, and user-friendly.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7">
+                        <div className="grid grid-cols-2 place-content-center">
+                            <div className="text-left p-10 ">
+                                <h3 className="text-xl font-bold text-slate-800 ">Super-fast and SEO-optimized blogs built for developers and tech enthusiasts.</h3>
+                                <p className="text-md font-md text-slate-500 ">.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div className="grid grid-flow-row-dense grid-cols-3 gap-10">
+
+                    <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7 col-span-2">
+                        <div className="grid grid-cols-2 place-content-center">
+                            <div className="text-left p-10 ">
+                                <h3 className="text-xl font-bold text-slate-800 ">Super-fast and SEO-optimized blogs built for developers and tech enthusiasts.</h3>
+                                <p className="text-md font-md text-slate-500 ">Hashnode blogs are fast, up-to-date with SEO techniques, and score above 90 on all Lighthouse parameters. They're simple, elegant, and user-friendly.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="bg-slate-100 rounded-3xl border-slate-200 leading-7">
+                        <div className="grid grid-cols-2 place-content-center">
+                            <div className="text-left p-10 ">
+                                <h3 className="text-xl font-bold text-slate-800 ">Super-fast and SEO-optimized blogs built for developers and tech enthusiasts.</h3>
+                                <p className="text-md font-md text-slate-500 ">.</p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            </div>
+        </>
+    )
+}
+export default Info
--- hashnode/src/pages/Home.jsx
+++ hashnode/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header"
 import Imgs from "../components/atoms/Imgs"
 import Brands from "../components/atoms/Brands"
+import Info from "../components/Info"
 
 function Home() {
     return (
@@ -14,9 +15,11 @@ function Home() {
 
 
             <div className="bg-white myShadow">
-
                 <Brands></Brands>
+                <Info></Info>
             </div>
+
+
         </>
     )
 }
