12b1961eaa2e7d6adc381f518112116c55ae7900
jessicaborsetto <bjessi2002@gmail.com>
2024-03-14 16:35:52 +0100

    clone

---
+++ hashnode/src/components/Pro.jsx
@@ -0,0 +1,11 @@
+function Pro(){
+    return(
+        <>
+        <div>
+            <h3>HASHNODE PRO</h3>
+        </div>
+        </>
+    )
+}
+
+export default Pro
--- hashnode/src/pages/Home.jsx
+++ hashnode/src/pages/Home.jsx
@@ -6,34 +6,26 @@ import News from "../components/News"
 import InfoNews from "../components/InfoNews"
 import Rating from "../components/Rating"
 import Teams from "../components/Teams"
+import Pro from "../components/Pro"
 
 function Home() {
     return (
         <>
             <Header></Header>
-
-        
-
             <Imgs></Imgs>
-      
-
-
             <div className="bg-white myShadow">
                 <Brands></Brands>
                 <Info></Info>
                 <News></News>
                 <InfoNews></InfoNews>
             </div>
-            
             <div className="bg-black">
-
                 <Rating></Rating>
             </div>
-            
             <Teams></Teams>
-
-
-
+            <div className="proSection">
+                <Pro></Pro>
+            </div>
         </>
     )
 }
